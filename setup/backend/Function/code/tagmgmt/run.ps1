19f8e554e2d8069cb98703580e682ea9e9d3f572
Jose Fehse <jose@fehse.ca>
2023-09-29 16:32:13 -0400

    little detail

--- setup/backend/Function/code/tagmgmt/run.ps1
+++ setup/backend/Function/code/tagmgmt/run.ps1
@@ -147,7 +147,7 @@ else
     "No resources provided."
 }
 $body = "This HTTP triggered function executed successfully. $($resources.count) were altered ($action)."
-Associate values to output bindings by calling 'Push-OutputBinding'.
+#Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
     Body = $body
