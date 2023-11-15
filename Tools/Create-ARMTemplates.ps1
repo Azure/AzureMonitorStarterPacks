67c9c7deb3d283833f516b8321a0c3d585a54381
Jose Fehse <jose@fehse.ca>
2023-11-15 10:01:56 -0500

    updated regions

--- Tools/Create-ARMTemplates.ps1
+++ Tools/Create-ARMTemplates.ps1
@@ -1,4 +1,15 @@
 # Get all the subfolders recursively
+# First update main solution files
+$mainMonstarPacksFiles = @(
+    "./setup/CustomSetup/monstar.bicep"
+    "./Packs/IaaS/AllIaaSPacks.bicep"
+) 
+foreach ($file in $mainMonstarPacksFiles) {
+    Set-Location -Path $file.DirectoryName
+    bicep build monitoring.bicep   
+}
+# Then update packs.
+Set-Location -Path ./Packs
 $subfolders = Get-ChildItem -Path . -Recurse -Directory
 $filelist= Get-ChildItem -Path . -Include "monitoring.bicep" -Recurse
 # Loop through each subfolder
