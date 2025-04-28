6726f065eb23db5c0278dab1f7eac65940ee4d1d
jose.fehse <jose.fehse@microsoft.com>
2025-04-28 12:37:42 -0400

    fix: Update pack content URL to fetch from GitHub repository

--- setup/backend/Function/code/Modules/common/common.psm1
+++ setup/backend/Function/code/Modules/common/common.psm1
@@ -1281,8 +1281,9 @@ function new-pack {
         
     )
     $modulesRoot = "./modules"
-    $packContentURL=
-    $packlist=get-content ./packs/packsdef.json | ConvertFrom-Json -Depth 15
+    $packContentURL= "https://raw.githubusercontent.com/FehseCorp/AzureMonitorStarterPacks/refs/heads/V3-SubDep/Packs/PacksDef.json"
+    $packlist = (Invoke-WebRequest -Uri $packContentURL -UseBasicParsing).content | ConvertFrom-Json -Depth 15
+    #$packlist=get-content ./packs/packsdef.json | ConvertFrom-Json -Depth 15
     $packlist.Packs | Where-Object { $_.Tag -eq $packtag }
     $packlist.Packs | Where-Object { $_.Tag -eq $packtag } | ForEach-Object {
         $pack = $_
