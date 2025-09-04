583970dbb5d3d9d4b383d7594a660bfd980bbc16
jose.fehse <jose.fehse@microsoft.com>
2025-09-04 09:01:00 -0400

    Pre-V3-Update1

---
+++ V2/setup/discovery/Windows/client/install.ps1
@@ -0,0 +1,11 @@
+﻿# Create discovery folder
+$DiscoveryFolder="c:\windowsAzure\Discovery"
+if ((get-item $discoveryFolder -ErrorAction SilentlyContinue)) { 
+    "$runTime : Ok.Folder already exists" | out-file DiscoverLog.txt -Append
+}
+else {
+    mkdir $DiscoveryFolder
+}
+#Expand-Archive .\discover.zip
+copy discover.ps1 $DiscoveryFolder
+Register-ScheduledTask -Xml (get-content ./DiscoveryTask.xml | out-string) -TaskName "Monstar Packs Discovery" -Force -User System -TaskPath "\"
