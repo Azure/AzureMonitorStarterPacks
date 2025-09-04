6fc559aabbf0ed3fb3dbe0ad628dad07665f56b5
jose.fehse <jose.fehse@microsoft.com>
2025-09-04 09:10:48 -0400

    removing V2 information

--- V2/Docs/LogicApp.md
+++
@@ -1,7 +0,0 @@
-# Logic App
-
-Used to broker the function apps when called from the workbook through ARM Actions.
-
-## Configuration
-
-![LogicApp](image-21.png)
--- V2/setup/discovery/Linux/client/discovery.sh
+++
@@ -1,4 +0,0 @@
-# Runtime,Type, OS, Name,Version
-# get Linux OS version from bash
-OSVERSION=$(cat /etc/os-release | grep PRETTY_NAME | cut -d'"' -f2)
-dpkg --list | awk '/^ii/ {print strftime("%Y-%m-%dT%H:%M:%S%z"),"application","Linux",ENVIRON["OSVERSION"],$2,$3}' | sort | sed 's/ /,/g' | sed '1i name,version' >> /opt/microsoft/discovery/discoveredapps.csv
